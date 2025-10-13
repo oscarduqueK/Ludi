60a12859b70995cb662f3c0429958eed0fea63e5
SrOscar <oscar.duque@enti.cat>
2025-10-13 11:38:58 +0200

    Scene caller created
    In PinBall Scene

---
+++ Assets/Project/Scripts/callScene.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class callScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //if (testSingleton.Instance == null)
+        //{
+        //    Instance = this;
+        //    DontDestroyOnLoad(gameObject); // opcional, si quieres que sobreviva a los cambios de escena
+        //}
+        //else
+        //{
+        //    Destroy(gameObject);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Load scene 1
+        Scene scene = SceneManager.GetSceneByName("MenuPause");
+        if (Input.GetKeyDown(KeyCode.Escape) && !scene.isLoaded)
+        {
+            SceneManager.LoadScene("MenuPause", LoadSceneMode.Additive);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && scene.isLoaded)
+        {
+            SceneManager.UnloadSceneAsync("MenuPause");
+        }
+    }
+}
