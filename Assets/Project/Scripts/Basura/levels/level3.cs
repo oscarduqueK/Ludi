6aa3e80c0a8a05a731026b0cf91d01ee9ade67da
SrOscar <oscar.duque@enti.cat>
2025-10-15 20:11:44 +0200

    niveles añadidos y mejora de la lógica de la basura

--- Assets/Project/Scripts/Basura/levels/level3.cs
+++ Assets/Project/Scripts/Basura/levels/level3.cs
@@ -1,10 +1,10 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
-public class level1 : levelConfig
+public class level3 : levelConfig
 {
     public override void SetupLevel(trashmanagement generator)
     {
-        Debug.Log("Nivel 1 configurado");
+        Debug.Log("Nivel 2 configurado");
     }
 
     public override void SpawnTrash(trashmanagement generator)
@@ -12,15 +12,15 @@ public class level1 : levelConfig
         //Basura necesaria para ganar
         GameManager.Instance.requiredTrashToWin = 5;
 
-        //Intervalo y verificación de funcionamiento
+        //Intervalo y verificaciÃ³n de funcionamiento
         if (generator.trashPrefabs.Count == 0) return;
-        generator.spawnInterval = 3.5f;
+        generator.spawnInterval = 3f;
 
         //Gestion de los spawnPoints
-        generator.spawnPointIndex = Random.Range(2, generator.spawnPoint.Count -2);
-        Transform indexSpanwPoint = generator.spawnPoint[generator.spawnPointIndex]; 
+        Transform indexSpanwPoint = generator.spawnPoint[generator.spawnPointIndex];
+        generator.spawnPointIndex = Random.Range(1, generator.spawnPoint.Count - 1);
 
-        //Gestión de los Prefabs 
+        //GestiÃ³n de los Prefabs 
         int index = Random.Range(0, generator.trashPrefabs.Count);
         GameObject trash = Object.Instantiate(generator.trashPrefabs[index], indexSpanwPoint.position, Quaternion.identity);
     }
