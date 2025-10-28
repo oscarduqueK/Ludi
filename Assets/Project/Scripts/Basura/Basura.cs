b6addcc45a0b9efce69665bd332e18f5eea8baf7
Carlinchos <carlos.blasco@enti.cat>
2025-10-28 20:09:43 +0100

    añado muhchas traducciones, sonidos y cambios de estetica

--- Assets/Project/Scripts/Basura/Basura.cs
+++ Assets/Project/Scripts/Basura/Basura.cs
@@ -1,21 +1,43 @@
-using JetBrains.Annotations;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 
 public class Basura : MonoBehaviour
 {
+    [Header("Audio")]
+    public AudioClip hitByBallSound;
+    public AudioClip hitByOtherSound;
+    public AudioMixerGroup outputMixerGroup;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.playOnAwake = false;
+        audioSource.outputAudioMixerGroup = outputMixerGroup;
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ball"))
         {
-            Destroy(this.gameObject);
+            if (hitByBallSound != null)
+                audioSource.PlayOneShot(hitByBallSound);
+
             GameManager.Instance.AddTrash(1);
+            Destroy(gameObject, 0.05f);
         }
-        else if (collision.gameObject.CompareTag("Rampa") || collision.gameObject.CompareTag("Flipper") || collision.gameObject.CompareTag("Ground"))
+        else if (collision.gameObject.CompareTag("Rampa") ||
+                 collision.gameObject.CompareTag("Flipper") ||
+                 collision.gameObject.CompareTag("Ground"))
         {
+            if (hitByOtherSound != null)
+                audioSource.PlayOneShot(hitByOtherSound);
+
             GameManager.Instance.RestarVida();
-            Destroy(this.gameObject);
+            Destroy(gameObject, 0.05f);
         }
     }
 }
-
-
